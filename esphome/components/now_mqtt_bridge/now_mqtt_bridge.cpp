a1d5218a56c3ebf34402caabd22f15330bcfb3db
Justin Decker <justin@ufire.co>
2023-06-01 16:09:06 -0700

    check for espnow message format

--- esphome/components/now_mqtt_bridge/now_mqtt_bridge.cpp
+++ esphome/components/now_mqtt_bridge/now_mqtt_bridge.cpp
@@ -33,6 +33,11 @@ namespace esphome
             char *tokens[13];
             int argc;
             split(tokens, &argc, received_string, ':', 1);
+
+            // if we didn't get 11 elements, this wasn't a message from our sensors
+            if (argc != 11)
+                return;
+
             ESP_LOGI(TAG, "line rcv: %s:%s:%s:%s:%s:%s:%s:%s:%s:%s:%s", tokens[0], tokens[1], tokens[2], tokens[3], tokens[4], tokens[5], tokens[6], tokens[7], tokens[8], tokens[9], tokens[10]);
             if (strlen(tokens[1]) != 0)
             {
